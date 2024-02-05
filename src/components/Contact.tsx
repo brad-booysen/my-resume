b13d91c627ca78a9bad9e6c696952f2e8ef3a96f
Brad Booysen <45187227+brad-booysen@users.noreply.github.com>
2024-02-05 18:12:23 +1300

    Send message from contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,57 +1,49 @@
-import { useState } from "react"
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import classes from './Contact.module.css'
+
 
 function Contact() {
 
-    const [formStatus, setFormStatus] = useState('Send')
+    const form = useRef();
 
-    const onSubmit = (e: React.SyntheticEvent) => {
-        e.preventDefault()
-        setFormStatus('Submitting...')
-        const target = e.target as typeof e.target & {
-            name: { value: string };
-            email: { value: string };
-            message: { value: string };
-        };
-        const name = target.name.value
-        const email = target.email.value
-        const message = target.message.value
-        const comFom = {
-            name: name,
-            email: email,
-            message: message
-        }
-        console.log(comFom)
-    }
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_h2jbq2n', 'contact_form', form.current, {
+                publicKey: 'OHtvf-JlKYMh2O_43',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
 
     return (
-        <div className="container mt-5" id={classes.form}>
+        <div className='container mt-5' id={classes.form}>
             <h2 className="mb-3">Contact Me</h2>
-            <form onSubmit={onSubmit}>
+            <form ref={form} onSubmit={sendEmail}>
                 <div className="mb-3">
-                    <label className="form-label" htmlFor="name">
-                        Name
-                    </label>
-                    <input className="form-control" type="text" id="name" required />
+                    <label htmlFor="user_name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="user_name" name="user_name" />
                 </div>
                 <div className="mb-3">
-                    <label className="form-label" htmlFor="email">
-                        Email
-                    </label>
-                    <input className="form-control" type="email" id="email" required />
+                    <label htmlFor="user_email" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="user_email" name="user_email" />
                 </div>
                 <div className="mb-3">
-                    <label className="form-label" htmlFor="message">
-                        Message
-                    </label>
-                    <textarea className="form-control" id="message" required />
+                    <label htmlFor="message" className="form-label">Message</label>
+                    <textarea className="form-control" id="message" name="message" rows={3} />
                 </div>
-                <button className="btn btn-success" type="submit">
-                    {formStatus}
-                </button>
+                <button type="submit" className="btn btn-success">Send</button>
             </form>
         </div>
-    )
+    );
 }
 
 export default Contact
