2d5dd2dc7ce4da749dff98790b62c531c1f30203
Brad B <brad@Brads-MacBook-Air.local>
2024-04-16 05:59:50 +1200

    Refactor comments

--- server.js
+++ server.js
@@ -6,19 +6,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import cors from 'cors';
-
 dotenv.config();
 
 const app = express();
 app.use(cors());
 
-// listening for port 3000
-app.listen(3000, () => console.log(`Server is running on ${port}`))
-
 const url = 'https://q3xubdzzt8.execute-api.us-east-1.amazonaws.com/items'
 const apiKey = "x147949773dfc1837f8569apf1"
 
-// API requests
+// listening for port 3000
+app.listen(3000, () => console.log(`Server is running on ${port}`))
+
+// Fetch visitor count from db
 app.get('/vistor-count', (req, res) => {
     const requestOptions = {
         method: 'GET',
@@ -35,7 +34,7 @@ app.get('/vistor-count', (req, res) => {
     });
 })
 
-// Log new visitor
+// Log new visitor in db
 app.put('/send-data', async (req, res) => {
 
     try {
