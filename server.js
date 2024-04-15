b3946747d8596d6a0212f785883023c9ca027045
Brad B <brad@Brads-MacBook-Air.local>
2024-04-16 07:07:14 +1200

    Secure api key

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 // defining the server port
-const port = 3000;
+const port = 3000
 
 // initializing installed dependencies
-import express from 'express';
-import dotenv from 'dotenv';
-import axios from 'axios';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-dotenv.config();
+import express from 'express'
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+dotenv.config()
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 app.listen(3000, () => console.log(`Server is running on ${port}`))
 
 const url = 'https://q3xubdzzt8.execute-api.us-east-1.amazonaws.com/items'
-const apiKey = "x147949773dfc1837f8569apf1"
+const apiKey = process.env.VITE_API_KEY
 
 // Fetch visitor count from db
 app.get('/vistor-count', (req, res) => {
@@ -27,17 +27,16 @@ app.get('/vistor-count', (req, res) => {
         },
     }
     axios.request(requestOptions)
-    .then(function (response) {
-        res.json(response.data)
-    }).catch(function (error) {
-        console.error(error);
-    });
+        .then(function (response) {
+            res.json(response.data)
+        }).catch(function (error) {
+            console.error(error);
+        });
 })
 
 // Log new visitor in db
 app.put('/send-data', (req, res) => {
 
-    // Data to send
     const dataToSend = {
         id: req.body.id
     }
